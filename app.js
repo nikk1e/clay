fc4c8f1abfdc2c0eb59234df4a4a6c2637fdaf84
Benjamin Norrington <bnorrington@uss.co.uk>
2014-09-24 18:35:30 +0100

    inital express server

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var project = require('./routes/project');
+
 var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 
 app.use(express.static(path.join(__dirname,'public')));
 
+app.use('/:area/:project/', project);
+
 app.set('port', process.env.PORT || 3000);
 
-var server = app.listen(app.get('port'), function() { 
+var server = app.listen(app.get('port'), function() {
 	console.log('Server listening on port' + server.address().port); 
 });
 
