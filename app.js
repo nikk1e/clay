360cb582b13ac316cf628e1c82151328986c21da
Benjamin Norrington <bnorrington@uss.co.uk>
2014-09-25 17:32:29 +0100

    start of file menu

--- app.js
+++ app.js
@@ -4,8 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
-var project = require('./routes/project');
+var base = process.env.DATAPATH || path.join(__dirname, 'data');
+
+//create data dir if missing
+try {
+    fs.mkdirSync(base);
+} catch (e) {
+    if (e.code != 'EEXIST') throw e;
+}
+
+var project = require('./routes/project')(base);
+var area = require('./routes/area')(base);
 
 var app = express();
 
@@ -21,6 +32,38 @@ app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/:area/:project/', project);
+app.use('/', area);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
 
 app.set('port', process.env.PORT || 3000);
 
--- routes/project.js
+++ routes/project.js
@@ -1,19 +1,110 @@
 var express = require('express');
-var path = require('path');
+var join = require('path').join;
+var git = require('git-node');
+var modes = require('js-git/lib/modes');
+//var async = require('async');
 var router = express.Router({ mergeParams: true });
 
-function tree(req, res) {
-	return res.send(req.params);
+var base;
+
+function tree(req, res, next) {
+	var area = req.params.area;
+	var project = req.params.project;
+	var repo = git.repo(join(base, area, project + '.git'));
+	var rawPath = req.params[0];
+	var commit = req.params.commit || 'master';
+	var path = rawPath
+		.slice(1)
+		.split('/')
+		.filter(function(p) { return p.length > 0; });
+	var last_commit;
+	//var files = [];
+	//var outstanding = 0;
+	//var broken = false;
+
+	path.reverse();
+
+	//function done() { if (!broken) res.send({files:files, path:path}); }
+	var baseUrl = '/' + area + '/' + project;
+
+	function done(tree) { 
+		tree = tree.map(function(p) {
+			var isDir = p.mode === modes.tree;
+			return {
+				name: p.name,
+				isDir: isDir,
+				url: baseUrl + (isDir ? '/tree/' : '/edit/') + commit + rawPath + '/' + p.name 
+			}
+		});
+
+		res.render('tree', {title: rawPath, tree: tree, commit: last_commit});
+		//res.send({tree: tree, commit: last_commit}); 
+
+	}
+
+	function loadTree(hash) {
+		//if (broken) return;
+		//outstanding++;
+		repo.loadAs('tree', hash, function(err, tree) {
+			var dir;
+			if (err) {
+				//broken = true;
+				return next(err);
+			}
+			//Array.prototype.push.apply(files, tree)
+			//files = tree;
+			if ((dir = path.pop())) {
+				for (var i = tree.length - 1; i >= 0; i--) {
+					if (tree[i].name === dir) {
+						if (tree[i].mode === modes.tree) {
+							return loadTree(tree[i].hash);
+						}
+					}
+				}
+				return next(new Error('Directory not found ' + rawPath));
+			} else {
+				done(tree);
+			}
+			//
+			//outstanding--;
+			//if (outstanding <= 0) done();
+		});
+	}
+
+	function loadCommit(hash) {
+		//if (broken) return;
+		//outstanding++;
+		repo.loadAs('commit', hash, function(err, commit) {
+			if (err) {
+				//broken = true;
+				return next(err);
+			}
+			last_commit = commit;
+			loadTree(commit.tree);
+			//if (commit.parents) {
+			//	commit.parents.forEach(loadCommit);
+			//}
+			//outstanding--;
+			//if (outstanding <= 0) done();
+		});
+	}
+
+	loadCommit(commit);
 }
 
-router.get('/', function(req, res) {
+router.get('/', function(req, res, next) {
   req.params.commit = 'master';
   req.params[0] = '';
-  return tree(req, res);
+  return tree(req, res, next);
 });
 
-router.get('/tree/:commit*', function(req, res) { //* is req.params[0]
-  return tree(req, res);
+router.get('/tree/:commit*', function(req, res, next) { //* is req.params[0]
+  return tree(req, res, next);
+});
+
+
+router.get('/commits/:branch*', function(req, res, next) {
+	//history of commits
 });
 
 router.get('/edit/:commit*', function(req, res) { //* is req.params[0]
@@ -25,8 +116,11 @@ router.get('/edit/:commit*', function(req, res) { //* is req.params[0]
   return res.sendfile('index.html', { root: 'public'});
 });
 
-router.post('/edit/:commit*', function(req, res) {
+router.post('/edit/:commit*', function(req, res, next) {
 	//expect json encoded commit tree
 });
 
-module.exports = router;
+module.exports = function(path) {
+  base = path;
+  return router;
+};
